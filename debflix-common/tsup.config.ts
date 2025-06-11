1e6ea38683b9cd37f8d8309a41c4555735d7e3cb
GalYazdi <galyazdi11@gmail.com>
2025-06-11 13:22:57 +0300

    Refactors module structure and removes unused code

--- debflix-common/tsup.config.ts
+++ debflix-common/tsup.config.ts
@@ -1,10 +1,22 @@
-import { defineConfig } from 'tsup';
- 
-export default defineConfig({
-    format: ['cjs', 'esm'],
-    entry: ['./src/index.ts'],
-    dts: true,
+import { defineConfig } from "tsup";
+
+export default defineConfig([
+  {
+    entry: ["packages/schemas/index.ts"],
+    outDir: "dist/schemas",
+    format: ["cjs", "esm"],
     shims: true,
     skipNodeModulesBundle: true,
+    dts: true,
     clean: true,
-});
+  },
+  {
+    entry: ["packages/types/index.ts"],
+    outDir: "dist/types",
+    format: ["cjs", "esm"],
+    shims: true,
+    skipNodeModulesBundle: true,
+    dts: true,
+    clean: true,
+  },
+]);
