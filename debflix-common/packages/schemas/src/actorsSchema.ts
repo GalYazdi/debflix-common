21fe819ee8f05e685f5ad93c0d55df01f2d2d309
GalYazdi <galyazdi11@gmail.com>
2025-07-22 01:17:09 +0300

    Updates actors schema to include firstName, lastName, and gender

--- debflix-common/packages/schemas/src/actorsSchema.ts
+++ debflix-common/packages/schemas/src/actorsSchema.ts
@@ -3,13 +3,15 @@ import { moviesSchema } from "./moviesSchema";
 
 export const actorsSchema = z
   .object({
-    name: z.string(),
+    firstName: z.string(),
+    lastName: z.string(),
     birthDate: z
       .number()
       .int()
       .min(new Date("1900-01-01").getTime())
       .max(Date.now()),
     movies: z.array(moviesSchema).optional(),
+    gender: z.enum(["male", "female"]),
   })
   .strict();
 
